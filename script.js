b4d17ebbbfca6dbf3e627c6559e01b6080554702
PEGOMEU <gabrielgomes24032004@gmail.com>
2026-02-05 15:44:39 -0300

    Save form data to localStorage on submit

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 document.getElementById("formulario").addEventListener("submit", function(e){
   e.preventDefault();
-  alert("Atendimento registrado com sucesso!");
+
+  let dados = {
+    nome: this[0].value,
+    whatsapp: this[1].value,
+    servico: this[2].value,
+    data: this[3].value,
+    valor: this[4].value,
+    pagamento: this[5].value,
+    funcionaria: this[6].value,
+    comissao: this[7].value,
+    obs: this[8].value
+  };
+
+  let lista = JSON.parse(localStorage.getItem("atendimentos")) || [];
+  lista.push(dados);
+  localStorage.setItem("atendimentos", JSON.stringify(lista));
+
+  alert("Atendimento salvo!");
+  this.reset();
 });
